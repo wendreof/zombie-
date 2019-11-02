562969246629a7ef75c31e8758fafeffbefe9905
Wendreo <wendreolf@gmail.com>
2019-11-01 23:47:17 -0300

    :rocket: Mapping new elements

--- pages/movie.js
+++ pages/movie.js
@@ -1,11 +1,12 @@
 module.exports = {
     elements: {
         addButton: '.movie-add',
+        movieForm: '#movie-form',
         titleInput: 'input[name=title]',
         statusSelect: 'input[placeholer=Status]',
         yearInput: 'input[name=year]',
         dateInput: 'input[name=release_date]',
-        castInput = '.cast',
+        castInput: '.cast',
         plotInput: 'textarea[name=overview]',
         createButton: '#create-movie'
     }
--- tests/sign-up/new_movie.test.js
+++ tests/sign-up/new_movie.test.js
@@ -2,7 +2,8 @@ let movieData = {}
 
 module.exports = {
 
-    'New movie': function(browser) {
+    before: function(browser) {
+
         movieData = {
             title: 'Resident Evil',
             status: 'Available',
@@ -17,10 +18,16 @@ module.exports = {
             cover: 'resisent-evil-2002.jpg',
             plot: 'The Alice and squad mission is to turn down the Red Queen and colect datas',
         }
+
+        let login = browser.page.login()
+        login.with('zumbi@dospalmares.com.br', 'pwd123')
     },
 
     'When I try to register a movie': function(browser) {
-        console.log(movieData)
-    }
+        let movie = browser.page.movie()
 
+        movie
+            .click('@addButton')
+            .waitForElementVisible('@movieForm', 3000)
+    }
 }
