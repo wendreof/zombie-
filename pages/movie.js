5b30a456979d23cdcd91d17406cb7de560b62ba6
Wendreo <wendreolf@gmail.com>
2019-11-01 23:33:35 -0300

    :rocket: Mapping new elements

---
+++ pages/movie.js
@@ -0,0 +1,12 @@
+module.exports = {
+    elements: {
+        addButton: '.movie-add',
+        titleInput: 'input[name=title]',
+        statusSelect: 'input[placeholer=Status]',
+        yearInput: 'input[name=year]',
+        dateInput: 'input[name=release_date]',
+        castInput = '.cast',
+        plotInput: 'textarea[name=overview]',
+        createButton: '#create-movie'
+    }
+}
