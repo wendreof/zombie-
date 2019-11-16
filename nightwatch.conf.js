450006041b8c19ef41fe921f05deeb56366f395d
Wendreo <wendreolf@gmail.com>
2019-11-16 02:49:42 -0200

    :rocket: Working with headless

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,6 +1,10 @@
 //require('babel-core/register')
 
 const chromedriver = require('chromedriver')
+require('geckodriver')
+
+const testURL = 'http://localhost:5000';
+const defaultTimeout = 15000;
 
 module.exports = {
     src_folders: ['tests'],
@@ -10,8 +14,6 @@ module.exports = {
 
     webdriver: {
         start_process: true,
-        server_path: chromedriver.path,
-        port: 9515
     },
 
     test_workers: {
@@ -21,12 +23,50 @@ module.exports = {
 
     test_settings: {
         default: {
-            launch_url: "http://localhost:5000",
+            launch_url: testURL,
             globals: {
-                waitForConditionTimeout: 15000
+                waitForConditionTimeout: defaultTimeout
+            },
+            webdriver: {
+                server_path: chromedriver.path,
+                port: 9515
             },
             desiredCapabilities: {
                 browserName: "Chrome"
+            }
+        },
+
+        headless: {
+            launch_url: testURL,
+            globals: {
+                waitForConditionTimeout: defaultTimeout
+            },
+            webdriver: {
+                server_path: chromedriver.path,
+                port: 9515
+            },
+            desiredCapabilities: {
+                browserName: "Chrome",
+                chromeOptions: {
+                    w3c: false,
+                    args: ['--headless', '--no-sandbox']
+                }
+            }
+        },
+
+
+        firefox: {
+            launch_url: testURL,
+            globals: {
+                waitForConditionTimeout: defaultTimeout
+            },
+            webdriver: {
+                server_path: './node_modules/.bin/geckodriver',
+                port: 4444
+            },
+            desiredCapabilities: {
+                browserName: "firefox",
+                acceptInsecureCerts: true
             }
         },
 
